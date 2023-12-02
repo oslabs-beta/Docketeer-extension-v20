a359e02e1a5b36d5ebe8e3a0060ef47027bf0409
Jade Chan <jade.melissa.chan@gmail.com>
2023-12-02 16:25:17 -0500

    updated nav-bar and added hamburger menu

--- ui/src/components/Images/Images.tsx
+++ ui/src/components/Images/Images.tsx
@@ -6,6 +6,7 @@ import globalStyles from '../global.module.scss';
 import { ImageType } from 'types';
 import { fetchImages } from '../../reducers/imageReducer';
 import Client from '../../models/Client';
+import ImageCard from '../ImageCard/ImageCard';
 
 /**
  * @module | Images.tsx
@@ -14,22 +15,19 @@ import Client from '../../models/Client';
 
 export interface TestParams {
   imagesListTest?: ImageType[];
-} 
+}
 // eslint-disable-next-line react/prop-types
-const Images = (params?: TestParams ): React.JSX.Element => {
-  // const Images = () => {
-  // imagesList for testing purposes only
-  // * above comment left by previous iteration. resolved type errors in order to test
-  console.log("Running images function");
-  const reduxImagesList = useAppSelector((state) => state.images.imagesList);
-  const imagesList = params.imagesListTest ? params.imagesListTest : reduxImagesList;
-  // const [repo, setRepo] = useState('');
-  console.log("images list: ", imagesList);
+// optional TestParams for testing only
+const Images = (params?: TestParams): React.JSX.Element => {
+  console.log('Running images function');
+  // const reduxImagesList = useAppSelector((state) => state.images.imagesList);
+  // const imagesList = params.imagesListTest ? params.imagesListTest : reduxImagesList;
+
   const dispatch = useAppDispatch();
 
-  useEffect((): void => {
-    dispatch(fetchImages());
-  }, []);
+  // useEffect((): void => {
+  //   dispatch(fetchImages());
+  // }, []);
 
   const runImage = async (image: ImageType) => {
     const success = await Client.ImageService.runImage(
@@ -52,7 +50,7 @@ const Images = (params?: TestParams ): React.JSX.Element => {
           () => {
             runImage(image);
             dispatch(
-              createAlert(`Running ${image.Repository}...`, 5, "success")
+              createAlert(`Running ${image.Repository}...`, 5, 'success')
             );
           },
           () => {
@@ -60,7 +58,7 @@ const Images = (params?: TestParams ): React.JSX.Element => {
               createAlert(
                 `The request to run ${image.Repository} has been cancelled.`,
                 5,
-                "warning"
+                'warning'
               )
             );
           }
@@ -77,7 +75,7 @@ const Images = (params?: TestParams ): React.JSX.Element => {
           () => {
             removeImage(image.ID);
             dispatch(
-              createAlert(`Removing ${image.Repository}...`, 5, "success")
+              createAlert(`Removing ${image.Repository}...`, 5, 'success')
             );
           },
           () => {
@@ -85,7 +83,7 @@ const Images = (params?: TestParams ): React.JSX.Element => {
               createAlert(
                 `The request to remove ${image.Repository} has been cancelled.`,
                 5,
-                "warning"
+                'warning'
               )
             );
           }
@@ -94,54 +92,72 @@ const Images = (params?: TestParams ): React.JSX.Element => {
     }
   };
 
+  // mock images data
+  const imagesList = [
+    { 'ImageName': 'ducketeer', 'High': 10, 'Med': 15, 'Low': 5 },
+    { 'ImageName': 'kirby-db', 'High': 20, 'Med': 30, 'Low': 15 },
+    { 'ImageName': 'banana', 'High': 2, 'Med': 10, 'Low': 5 }
+  ];
+  console.log('images list: ', imagesList);
+
+  const renderedImages = [];
+
+  for(let i = 0; i < imagesList.length; i++) {
+    renderedImages.push(<ImageCard key={i} imgObj={imagesList[i]}/>)
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.availableImagesHolder}>
-        <h2>AVAILABLE IMAGES</h2>
-        <div className={styles.imageHolder}>
-          {imagesList.map((image, i: number) => {
-            return (
-              <div key={`image-${i}`} className={styles.imageCard}>
-                <div className={styles.textHolder}>
-                  <figure>
-                    <img
-                      className={styles.image}
-                      src={`https://d36jcksde1wxzq.cloudfront.net/54e48877dab8df8f92cd.png`}
-                    />
-                  </figure>
-                  <div>
-                    <h2>{image.Repository}</h2>
-                    <p>{image.Tag}</p>
-                    <p>{`Image ID: ${image.ID}`}</p>
-                    <p>{`Image Size: ${image.Size}`}</p>
-                  </div>
-                  <div className={styles.buttonHolder}>
-                    <div className={styles.buttonSpacer}>
-                      <button
-                        role="button"
-                        name="RUN"
-                        className={globalStyles.buttonSmall}
-                        onClick={() => runImageAlert(image)}
-                      >
-                        RUN
-                      </button>
-                      <button
-                        role="button"
-                        name="REMOVE"
-                        className={globalStyles.buttonSmall}
-                        onClick={() => removeImageAlert(image)}
-                      >
-                        REMOVE
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </div>
+    <div>
+      {renderedImages}
     </div>
+
+    // <div className={styles.wrapper}>
+    //   <div className={styles.availableImagesHolder}>
+    //     <h2>AVAILABLE IMAGES</h2>
+    //     <div className={styles.imageHolder}>
+    //       {imagesList.map((image, i: number) => {
+    //         return (
+    //           <div key={`image-${i}`} className={styles.imageCard}>
+    //             <div className={styles.textHolder}>
+    //               <figure>
+    //                 <img
+    //                   className={styles.image}
+    //                   src={`https://d36jcksde1wxzq.cloudfront.net/54e48877dab8df8f92cd.png`}
+    //                 />
+    //               </figure>
+    //               <div>
+    //                 <h2>{image.Repository}</h2>
+    //                 <p>{image.Tag}</p>
+    //                 <p>{`Image ID: ${image.ID}`}</p>
+    //                 <p>{`Image Size: ${image.Size}`}</p>
+    //               </div>
+    //               <div className={styles.buttonHolder}>
+    //                 <div className={styles.buttonSpacer}>
+    //                   <button
+    //                     role="button"
+    //                     name="RUN"
+    //                     className={globalStyles.buttonSmall}
+    //                     onClick={() => runImageAlert(image)}
+    //                   >
+    //                     RUN
+    //                   </button>
+    //                   <button
+    //                     role="button"
+    //                     name="REMOVE"
+    //                     className={globalStyles.buttonSmall}
+    //                     onClick={() => removeImageAlert(image)}
+    //                   >
+    //                     REMOVE
+    //                   </button>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         );
+    //       })}
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
