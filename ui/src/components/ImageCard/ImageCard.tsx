d6eba8864ad0593c166d03e57781a9446cff0c80
Jessica Grant <jessicarobyn10@gmail.com>
2023-12-05 11:14:58 -0500

    Styling Image Card

--- ui/src/components/ImageCard/ImageCard.tsx
+++ ui/src/components/ImageCard/ImageCard.tsx
@@ -25,19 +25,22 @@ function ImageCard({ imgObj, runImageAlert, removeImageAlert }: ImageCardProps):
 			</div>
 			
 			{/* vulnerability info + run / remove functionality: RIGHT SIDE */}
-			<div>
+			<div className={styles.imageInfo}>
+				
 				{/* RUN / REMOVE */}
 				<div>
 					<button onClick={() => runImageAlert(imgObj)}>run image</button>
 					<button onClick={() => removeImageAlert(imgObj)}>remove image</button>
 				</div>
+				
 				{/* VULNERABILITY */}
-				<div>
-					<text>Critical: {imgObj['Critical']}</text>
-					<text>High: {imgObj['High']}</text>
-					<text>Med: {imgObj['Med']}</text>
-					<text>Low: {imgObj['Low']}</text>
+				<div className={styles.imageVulnerabilities}>
+					<p className={styles.critical}>{imgObj['Critical']} Critical</p>
+					<p className={styles.high}>{imgObj['High']} High</p>
+					<p className={styles.med}>{imgObj['Med']} Med</p>
+					<p className={styles.low}>{imgObj['Low']} Low</p>
 				</div>
+		
 			</div>
 		
 		</div>
