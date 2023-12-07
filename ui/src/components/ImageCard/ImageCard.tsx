83f34c972027836612cb3af21064fddfc478c6f5
GiovanniCortes <giocortes19@gmail.com>
2023-12-07 15:15:02 -0500

    updated style of ImageCard.tsx

--- ui/src/components/ImageCard/ImageCard.tsx
+++ ui/src/components/ImageCard/ImageCard.tsx
@@ -20,7 +20,7 @@ interface ImageCardProps {
 function ImageCard({ imgObj, runImageAlert, removeImageAlert }: ImageCardProps): React.JSX.Element {
 
 	// initialize state variable to store vulnerabilities
-	const [scanObj, setScanObj] = useState({})
+	const [scanObj, setScanObj] = useState({Critical: '-', High: '-', Medium: '-', Low: '-'})
 
 	const getScan = async (scanName: string) => { 
 		// retrieve scan data
@@ -32,44 +32,49 @@ function ImageCard({ imgObj, runImageAlert, removeImageAlert }: ImageCardProps):
     return success;
   }
 
-	useEffect(() => { 		
-		getScan(imgObj.ScanName)
-	}, [])
+	// useEffect(() => { 		
+	// 	getScan(imgObj.ScanName)
+	// }, [])
 
 	return (
 		<div className={styles.imageCard}>
-			{/* image name: LEFT SIDE */}
-			<div>
-				<p>Image Name: {imgObj['Repository']}</p>
-			</div>
+
 			{/* vulnerability info + run / remove functionality: RIGHT SIDE */}
 			<div className={styles.imageInfo}>
-				{/* RUN / REMOVE */}
+				{/* image name: LEFT SIDE */}
 				<div>
-					<button className={styles.imgCardButton} onClick={() => runImageAlert(imgObj)}>run image</button>
-					<button className={styles.imgCardButton} onClick={() => removeImageAlert(imgObj)}>remove image</button>
+					<p className={styles.ImageName}>{imgObj['Repository']}</p>
 				</div>
 				
 				{/* VULNERABILITY */}
-				<div className={styles.imageVulnerabilities}>
-					<div className={styles.imgVulDiv}>
-						<p className={styles.critical}>Critical</p>
-						<p className={styles.critical}>{scanObj['Critical']} </p>
-					</div>
-					<div className={styles.imgVulDiv}>
-						<p className={styles.high}>High</p>
-						<p className={styles.high}>{scanObj['High']} </p>
-					</div>
-					<div className={styles.imgVulDiv}>
-						<p className={styles.medium}>Med</p>
-						<p className={styles.medium}>{scanObj['Medium']} </p>
-					</div>
-					<div className={styles.imgVulDiv}>
-						<p className={styles.low}>Low</p>
-						<p className={styles.low}>{scanObj['Low']} </p>
+				<div className={styles.VulnerabilitiesBlock}>
+					<p className={styles.VulnerabilitiesTitle}>Vulnerabilities</p>
+					<div className={styles.imageVulnerabilities}>
+						<div className={styles.imgVulDiv}>
+							<p className={styles.critical}>Critical</p>
+							<p className={styles.critical}>{scanObj['Critical']} </p>
+						</div>
+						<div className={styles.imgVulDiv}>
+							<p className={styles.high}>High</p>
+							<p className={styles.high}>{scanObj['High']} </p>
+						</div>
+						<div className={styles.imgVulDiv}>
+							<p className={styles.medium}>Med</p>
+							<p className={styles.medium}>{scanObj['Medium']} </p>
+						</div>
+						<div className={styles.imgVulDiv}>
+							<p className={styles.low}>Low</p>
+							<p className={styles.low}>{scanObj['Low']} </p>
+						</div>
 					</div>
 				</div>
 			</div>
+
+				{/* RUN / REMOVE */}
+				<div className={styles.buttons}>
+					<button className={styles.imgCardButton} onClick={() => runImageAlert(imgObj)}>RUN</button>
+					<button className={styles.imgCardButton} onClick={() => removeImageAlert(imgObj)}>DELETE IMAGE</button>
+				</div>
 		</div>
 	)
 }
