438ba37ec63f8f118c896e776b7c85542fa0015c
Jessica Grant <jessicarobyn10@gmail.com>
2023-12-16 11:39:03 -0500

    Create updateVulnerabilities reducer that updates state in the Redux store
    Co-authored-by: Giovanni Cortes <giocortes19@gmail.com>

--- ui/src/reducers/imageReducer.ts
+++ ui/src/reducers/imageReducer.ts
@@ -1,5 +1,5 @@
-import { PayloadAction, createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { ImagesStateType } from '../../ui-types';
+import { PayloadAction, createSlice, createAsyncThunk, current } from '@reduxjs/toolkit';
+import { ImagesStateType, VulnerabilityPayload } from '../../ui-types';
 import { ImageType } from '../../../types';
 import Client from '../models/Client';
 const initialState: ImagesStateType = {
@@ -10,14 +10,24 @@ export const fetchImages = createAsyncThunk(
   'containers/fetchImages',
   async () => {
     const result: ImageType[] = await Client.ImageService.getImages();
-    // const result: String[] = await Client.ImageService.getImages();
     return result;
   }
-)
+);
+
 export const imageSlice = createSlice({
   name: 'images',
   initialState,
-  reducers: {},
+  reducers: {
+    updateVulnerabilities(state, action: PayloadAction<VulnerabilityPayload>) {
+      console.log('action argument: ', action);
+      
+      // Updating the image object's Vulnerability property to have the scanned vulnerabilites
+      console.log('in updateVulnerabilities reducer TOP', current(state.imagesList))
+      const matchedImg = state.imagesList.find(imageObj => imageObj.ScanName === action.payload.scanName)
+      matchedImg.Vulnerabilities = action.payload.success
+      console.log('in updateVulnerabilities reducer BOTTOM', current(matchedImg))
+    }
+  },
   extraReducers(builder) {
     builder
       .addCase(
@@ -28,4 +38,5 @@ export const imageSlice = createSlice({
   }
 });
 
+export const {updateVulnerabilities} = imageSlice.actions
 export default imageSlice.reducer;
