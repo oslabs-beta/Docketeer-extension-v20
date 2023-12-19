9b778a851326ab0ee96fcf2fd3355dbb714ec680
GiovanniCortes <giocortes19@gmail.com>
2023-12-19 15:17:12 -0500

    Created deleteImage() reducer to update imagesList in the redux store.
    Co-authored-by: Jessica Grant <jessicarobyn10@users.noreply.github.com>

--- ui/src/reducers/imageReducer.ts
+++ ui/src/reducers/imageReducer.ts
@@ -3,7 +3,7 @@ import { ImagesStateType, VulnerabilityPayload } from '../../ui-types';
 import { ImageType } from '../../../types';
 import Client from '../models/Client';
 const initialState: ImagesStateType = {
-  imagesList: [],
+  imagesList: []
 };
 
 export const fetchImages = createAsyncThunk(
@@ -19,24 +19,39 @@ export const imageSlice = createSlice({
   initialState,
   reducers: {
     updateVulnerabilities(state, action: PayloadAction<VulnerabilityPayload>) {
-      console.log('action argument: ', action);
-      
+      // console.log('action argument: ', action);
+
       // Updating the image object's Vulnerability property to have the scanned vulnerabilites
-      console.log('in updateVulnerabilities reducer TOP', current(state.imagesList))
-      const matchedImg = state.imagesList.find(imageObj => imageObj.ScanName === action.payload.scanName)
-      matchedImg.Vulnerabilities = action.payload.success
-      console.log('in updateVulnerabilities reducer BOTTOM', current(matchedImg))
+      // console.log(
+      //   'in updateVulnerabilities reducer TOP',
+      //   current(state.imagesList)
+      // );
+      const matchedImg = state.imagesList.find(
+        (imageObj) => imageObj.ScanName === action.payload.scanName
+      );
+      matchedImg.Vulnerabilities = action.payload.success;
+      // console.log(
+      //   'in updateVulnerabilities reducer BOTTOM',
+      //   current(matchedImg)
+      // );
+    },
+    deleteImage(state, action: PayloadAction<string>) {
+      console.log('Before deleting image from the list: ', current(state.imagesList));
+      
+      // find the index of the image object with the action.payload
+      const imageIndex = state.imagesList.findIndex((imageObj) => imageObj.ID === action.payload)
+      // splice the store.imageList at the found index, delete 1, insert nothing
+      state.imagesList.splice(imageIndex, 1)
+      console.log('After deleting image from the list: ', current(state.imagesList));
+
     }
   },
   extraReducers(builder) {
-    builder
-      .addCase(
-        fetchImages.fulfilled, (state, action) => {
-          state.imagesList = action.payload;
-        }
-      )
-  }
+    builder.addCase(fetchImages.fulfilled, (state, action) => {
+      state.imagesList = action.payload;
+    });
+  },
 });
 
-export const {updateVulnerabilities} = imageSlice.actions
+export const {updateVulnerabilities, deleteImage} = imageSlice.actions
 export default imageSlice.reducer;
