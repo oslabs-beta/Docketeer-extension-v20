1809bdce26258ac97513bc3bd078b103bf148c57
QuanN <jeanquan98@gmail.com>
2024-03-09 01:42:47 -0600

    Added styles to vulnerability cards

--- ui/src/components/ImageCard/ImageCard.tsx
+++ ui/src/components/ImageCard/ImageCard.tsx
@@ -26,64 +26,62 @@ import { every } from 'd3';
 const ImageCard = ({ imgObj, runImageAlert, removeImageAlert, index }: ImageCardProps): React.JSX.Element => {
   const dispatch = useAppDispatch();
   const [done, setDone] = useState<Boolean>(false);
-	let critical, high, medium, low;
+  let critical, high, medium, low;
 
-	// get vulnerabilities directly from the store
-	let vulnerabilities =
+  // get vulnerabilities directly from the store
+  let vulnerabilities =
     useAppSelector((state) => state.images.imagesList[index].Vulnerabilities) ||
     false;
 
-	const getScan = async (scanName: string) => {
-
+  const getScan = async (scanName: string) => {
     try {
-			// retrieve scan data - Client.ImageService.getScan creates DDClient Request
-			// const vulnerabilityObj: ScanObject = await Client.ImageService.getScan(scanName);
+      // retrieve scan data - Client.ImageService.getScan creates DDClient Request
+      // const vulnerabilityObj: ScanObject = await Client.ImageService.getScan(scanName);
 
-			const scanObjectReturn: ScanReturn = await Client.ImageService.getScan(scanName);
+      const scanObjectReturn: ScanReturn = await Client.ImageService.getScan(
+        scanName
+      );
       const vulnerabilityObj: ScanObject = scanObjectReturn.vulnerabilites;
       if (!done) setDone(true);
 
-			console.log("scanObjectReturn JSON FOR GRYPE: ", scanObjectReturn);
-			console.log(`Success from getScan: ${scanName}`, vulnerabilityObj);
+      console.log('scanObjectReturn JSON FOR GRYPE: ', scanObjectReturn);
+      console.log(`Success from getScan: ${scanName}`, vulnerabilityObj);
 
-			// get the info from 4 levels
-			const everything: GrypeScan[] = scanObjectReturn.everything;
-			critical = everything.filter((el) => el.Severity === 'Critical');
-			high = everything.filter((el) => el.Severity === 'High');
-			medium = everything.filter((el) => el.Severity === 'Medium');
-			low = everything.filter((el) => el.Severity === 'Low');
+      // get the info from 4 levels
+      const everything: GrypeScan[] = scanObjectReturn.everything;
+      critical = everything.filter((el) => el.Severity === 'Critical');
+      high = everything.filter((el) => el.Severity === 'High');
+      medium = everything.filter((el) => el.Severity === 'Medium');
+      low = everything.filter((el) => el.Severity === 'Low');
 
-			// if the image failed to be scanned for vulnerabilities, update the image card state to have a default vulnerability object
-			if (vulnerabilityObj === undefined) {
-				const defaultVul: VulnerabilityPayload = {vulnerabilityObj:{ Critical: '-', High: '-', Medium: '-', Low: '-' }, scanName: scanName}
-				dispatch(updateVulnerabilities(defaultVul));
-				return;
+      // if the image failed to be scanned for vulnerabilities, update the image card state to have a default vulnerability object
+      if (vulnerabilityObj === undefined) {
+        const defaultVul: VulnerabilityPayload = {
+          vulnerabilityObj: { Critical: '-', High: '-', Medium: '-', Low: '-' },
+          scanName: scanName,
+        };
+        dispatch(updateVulnerabilities(defaultVul));
+        return;
       }
 
-			// create an object of type VulnerabilityPayload with the returned vulnerability object and the scanName
-			const updateVul: VulnerabilityPayload = { vulnerabilityObj, scanName }
-			// dispatch VulnerabilityPayload to update the imgObj in the store with the vulnerability info
-			dispatch(updateVulnerabilities(updateVul))
-			console.log('after reducuer invoked', imgObj)
-			return;
+      // create an object of type VulnerabilityPayload with the returned vulnerability object and the scanName
+      const updateVul: VulnerabilityPayload = { vulnerabilityObj, scanName };
+      // dispatch VulnerabilityPayload to update the imgObj in the store with the vulnerability info
+      dispatch(updateVulnerabilities(updateVul));
+      console.log('after reducuer invoked', imgObj);
+      return;
     } catch (error) {
       // Log error if failed
       console.log('getScan has failed to get vulnerabilities: ', error);
     }
-  }
+  };
 
-	// call getScan upon render for each card
-	useEffect(() => {
-    if (!vulnerabilities) getScan(imgObj.ScanName);
-    else setDone(true); // keep the green check
-  }, [])
+  const [dropdown, setDropdown] = useState(false);
 
-	const [dropdown, setDropdown] = useState(false);
-
-	const toggleDropdown = () => {
-		setDropdown(!dropdown);
-	}
-	const [dropDown, setDropDown] = useState({
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+  const [dropDown, setDropDown] = useState({
     critical: false,
     high: false,
     medium: false,
@@ -101,9 +99,27 @@ const ImageCard = ({ imgObj, runImageAlert, removeImageAlert, index }: ImageCard
     });
   };
 
+  // call getScan upon render for each card
+  useEffect(() => {
+    if (!vulnerabilities) getScan(imgObj.ScanName);
+    else setDone(true); // keep the green check
+  }, []);
 
-	return (
-    <div className={styles.imageCard}>
+  return (
+    <div
+      className={
+        done && Object.keys(vulnerabilities).length === 4
+          ? styles.imageCardCrit
+          : done && Object.keys(vulnerabilities).length === 3
+          ? styles.imageCardHigh
+          : done && Object.keys(vulnerabilities).length === 2
+          ? styles.imageCardMed
+          : done && Object.keys(vulnerabilities).length === 1
+          ? styles.imageCardLow
+          : done && Object.keys(vulnerabilities).length === 0
+          ? styles.imageCardDone
+          : styles.imageCard
+      }>
       {/* vulnerability info + run / remove functionality: RIGHT SIDE */}
       <div className={styles.imageInfo}>
         {/* image name: LEFT SIDE */}
@@ -187,22 +203,22 @@ const ImageCard = ({ imgObj, runImageAlert, removeImageAlert, index }: ImageCard
           {/* toggler drop down info of vulnerability type clicked */}
           {dropDown.critical && (
             <div className={styles.dropDown} id={`test${index}`}>
-              <p>Critical</p>
+              <p className={styles.dropDownText}>Critical</p>
             </div>
           )}
           {dropDown.high && (
             <div className={styles.dropDown} id={`test${index}`}>
-              <p>High</p>
+              <p className={styles.dropDownText}>High</p>
             </div>
           )}
           {dropDown.medium && (
             <div className={styles.dropDown} id={`test${index}`}>
-              <p>Medium</p>
+              <p className={styles.dropDownText}>Medium</p>
             </div>
           )}
           {dropDown.low && (
             <div className={styles.dropDown} id={`test${index}`}>
-              <p>Low</p>
+              <p className={styles.dropDownText}>Low</p>
             </div>
           )}
         </div>
